4c7a4578adf8082a2b708387ebeb7d1d704536df
mishakov7 <mishakov.dev@gmail.com>
2025-03-30 13:28:27 -0400

    Converted app to React instead of Next

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Dresser from './components/Dresser';
+
+function App() {
+
+  return (
+    <main>
+      <Dresser />
+    </main>
+  )
+}
+
+export default App
